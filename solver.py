632ae34dbb070a3ad223fe1c3a1b8ffbb4409311
vakane <vadim@Vadims-MBP.fios-router.home>
2020-06-04 00:52:04 -0400

    universal function

--- solver.py
+++ solver.py
@@ -7,13 +7,13 @@ DELTA_CLIP = 50.0
 
 
 class BSDESolver(object):
-    """The fully connected neural network model."""
-    def __init__(self, config, bsde):
+    """Generic neural net based solver model."""
+    def __init__(self, config, bsde, model):
         self.eqn_config = config.eqn_config
         self.net_config = config.net_config
         self.bsde = bsde
 
-        self.model = NonsharedModel(config, bsde)
+        self.model = model(config, bsde)
         self.y_init = self.model.y_init
         lr_schedule = tf.keras.optimizers.schedules.PiecewiseConstantDecay(
             self.net_config.lr_boundaries, self.net_config.lr_values)
@@ -61,6 +61,7 @@ class BSDESolver(object):
 
 
 class NonsharedModel(tf.keras.Model):
+    """Multiple neural nets f_t(x)"""
     def __init__(self, config, bsde):
         super(NonsharedModel, self).__init__()
         self.eqn_config = config.eqn_config
@@ -125,63 +126,51 @@ class FeedForwardSubNet(tf.keras.Model):
         x = self.dense_layers[-1](x)
         x = self.bn_layers[-1](x, training)
         return x
-
     
-class BSDESolver_GRU(object):
-    """RNN model"""
+
+class UFModel(tf.keras.Model):
+    """Universal function f(x, t)"""
     def __init__(self, config, bsde):
+        super().__init__()
         self.eqn_config = config.eqn_config
         self.net_config = config.net_config
         self.bsde = bsde
+        self.y_init = tf.Variable(np.random.uniform(low=self.net_config.y_init_range[0],
+                                                    high=self.net_config.y_init_range[1],
+                                                    size=[1])
+                                  )
+        self.z_init = tf.Variable(np.random.uniform(low=-.1, high=.1,
+                                                    size=[1, self.eqn_config.dim])
+                                  )
 
-        self.model = GRUModel(config, bsde)
-        self.y_init = self.model.y_init
-        lr_schedule = tf.keras.optimizers.schedules.PiecewiseConstantDecay(
-            self.net_config.lr_boundaries, self.net_config.lr_values)
-        self.optimizer = tf.keras.optimizers.Adam(learning_rate=lr_schedule, epsilon=1e-8)
+        self.subnet = FeedForwardSubNet(config)
 
-    def train(self):
-        start_time = time.time()
-        training_history = []
-        valid_data = self.bsde.sample(self.net_config.valid_size)
-
-        # begin sgd iteration
-        for step in range(self.net_config.num_iterations+1):
-            if step % self.net_config.logging_frequency == 0:
-                loss = self.loss_fn(valid_data, training=False).numpy()
-                y_init = self.y_init.numpy()[0]
-                elapsed_time = time.time() - start_time
-                training_history.append([step, loss, y_init, elapsed_time])
-                if self.net_config.verbose:
-                    logging.info("step: %5u,    loss: %.4e, Y0: %.4e,   elapsed time: %3u" % (
-                        step, loss, y_init, elapsed_time))
-            self.train_step(self.bsde.sample(self.net_config.batch_size))
-        return np.array(training_history)
-
-    def loss_fn(self, inputs, training):
+    def call(self, inputs, training):
         dw, x = inputs
-        y_terminal = self.model(inputs, training)
-        delta = y_terminal - self.bsde.g_tf(self.bsde.total_time, x[:, :, -1])
-        # use linear approximation outside the clipped range
-        loss = tf.reduce_mean(tf.where(tf.abs(delta) < DELTA_CLIP, tf.square(delta),
-                                       2 * DELTA_CLIP * tf.abs(delta) - DELTA_CLIP ** 2))
+        time_stamp = np.arange(0, self.eqn_config.num_time_interval) * self.bsde.delta_t
+        all_one_vec = tf.ones(shape=tf.stack([tf.shape(dw)[0], 1]), dtype=self.net_config.dtype)
+        y = all_one_vec * self.y_init
+        z = tf.matmul(all_one_vec, self.z_init)
 
-        return loss
+        for t in range(0, self.bsde.num_time_interval-1):
+            y = y - self.bsde.delta_t * (
+                self.bsde.f_tf(time_stamp[t], x[:, :, t], y, z)
+            ) + tf.reduce_sum(z * dw[:, :, t], 1, keepdims=True)
+            
+            _x = tf.concat([x[:, :, t + 1],
+                            t/self.bsde.num_time_interval * tf.ones((x.shape[0], 1), dtype=self.net_config.dtype)
+                           ], 1)
+            
+            z = self.subnet(_x, training) / self.bsde.dim
+        # terminal time
+        y = y - self.bsde.delta_t * self.bsde.f_tf(time_stamp[-1], x[:, :, -2], y, z) + \
+            tf.reduce_sum(z * dw[:, :, -1], 1, keepdims=True)
 
-    def grad(self, inputs, training):
-        with tf.GradientTape(persistent=True) as tape:
-            loss = self.loss_fn(inputs, training)
-        grad = tape.gradient(loss, self.model.trainable_variables)
-        del tape
-        return grad
-
-    @tf.function
-    def train_step(self, train_data):
-        grad = self.grad(train_data, training=True)
-        self.optimizer.apply_gradients(zip(grad, self.model.trainable_variables))
-
-
+        return y
+    
+    
 class GRUModel(tf.keras.Model):
+    """RNN Model"""
     def __init__(self, config, bsde):
         super().__init__()
         self.eqn_config = config.eqn_config
